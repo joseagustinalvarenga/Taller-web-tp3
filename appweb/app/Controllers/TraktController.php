4dcf0c167e4c797954b6a5416f6147722fdadab1
aalvarenga <aalvarenga@sancorseguros.com.ar>
2023-07-13 20:44:38 -0300

    genio

--- appweb/app/Controllers/TraktController.php
+++ appweb/app/Controllers/TraktController.php
@@ -3,50 +3,21 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use Config\Trakt;
 
 class TraktController extends Controller
 {
+    private $apiKey = "52724ba78d1d787ef10c7580921676b1cb1ee71fe6ffc10c6f9d68b404d8c19a";
+
     public function index()
     {
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, "https://private-anon-e2f01fae6a-trakt.apiary-proxy.com/movies/trending");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type: application/json",
-            "trakt-api-version: 2",
-            "trakt-api-key: 52724ba78d1d787ef10c7580921676b1cb1ee71fe6ffc10c6f9d68b404d8c19a" // Reemplaza TU_CLIENT_ID con tu API key de Trakt
-        ));
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        //var_dump($response);
-
-        // Decodifica la respuesta JSON
-        $movies = json_decode($response);
-
-        // Verificar si $movies es un array o un objeto válido
-        if (is_array($movies) || is_object($movies)) {
-            // Pasar los datos a la vista para mostrar el listado de películas
-            $data = [
-                'movies' => $movies
-            ];
-
-            return view('trakt/list', $data);
-        } else {
-            // Mostrar un mensaje de error o redireccionar a una página de error
-            echo "Error al obtener el listado de películas recomendadas";
-        }
+        $movies = $this->getRecommendedMovies();
+        return view('trakt/list', ['movies' => $movies]);
     }
 
-    private function getRecommendedMovies()
+    public function searchMovies($query)
     {
-        $apiKey = "52724ba78d1d787ef10c7580921676b1cb1ee71fe6ffc10c6f9d68b404d8c19a";
-        $url = "https://private-anon-e2f01fae6a-trakt.apiary-proxy.com/movies/trending";
+        $query = urlencode($query);
+        $url = "https://api.trakt.tv/search/movie,show?query=" . $query;
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -55,7 +26,30 @@ class TraktController extends Controller
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             "Content-Type: application/json",
             "trakt-api-version: 2",
-            "trakt-api-key: " . $apiKey
+            "trakt-api-key: " . $this->apiKey
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $results = json_decode($response);
+
+        // Pasar los resultados a la vista
+        return view('trakt/list', ['movies' => $results]);
+    }
+
+    private function getRecommendedMovies()
+    {
+        $url = "https://api.trakt.tv/movies/trending";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "trakt-api-version: 2",
+            "trakt-api-key: " . $this->apiKey
         ));
 
         $response = curl_exec($ch);
@@ -66,8 +60,7 @@ class TraktController extends Controller
 
     public function verComentarios($traktId)
     {
-        $apiKey = "52724ba78d1d787ef10c7580921676b1cb1ee71fe6ffc10c6f9d68b404d8c19a";
-        $url = "https://private-anon-e2f01fae6a-trakt.apiary-proxy.com/movies/" . $traktId . "/comments/highest";
+        $url = "https://api.trakt.tv/movies/" . $traktId . "/comments/highest";
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -76,15 +69,13 @@ class TraktController extends Controller
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             "Content-Type: application/json",
             "trakt-api-version: 2",
-            "trakt-api-key: " . $apiKey
+            "trakt-api-key: " . $this->apiKey
         ));
 
         $response = curl_exec($ch);
         curl_close($ch);
 
         $comments = json_decode($response);
-        //print_r($comments);
-        // Manipular los comentarios según tus necesidades
 
         return view('trakt/comments', ['comments' => $comments]);
     }
--- appweb/app/Views/trakt/list.php
+++ appweb/app/Views/trakt/list.php
@@ -14,14 +14,12 @@
 <body>
     <header class="barra_superior_home_page">
         <div class="imagen_header">
-                <a href="http://localhost/Taller-web-tp3/appweb/public/User/mostrar_login" id="link_imagen"><img src="<?php echo base_url('Taller-web-tp3/appweb/public/assets/img/logo.png'); ?>" id="imagen_boton_play"></a>
-            </div>
-            <div class="titulo">
-                <a href="http://localhost/Taller-web-tp3/appweb/public/User/mostrar_login" id="link_titulo"><h1>Video Trend</h1></a>
-            </div>
-            <div class="subti">
-                <p>Mira tus videos de Youtube como quieras</p>
-            </div>
+            <a href="http://localhost/Taller-web-tp3/appweb/public/User/mostrar_login" id="link_imagen">
+                <img src="<?php echo base_url('Taller-web-tp3/appweb/public/assets/img/logo.png'); ?>" id="imagen_boton_play"></a>
+        </div>
+        <div class="titulo">
+            <a href="http://localhost/Taller-web-tp3/appweb/public/User/mostrar_login" id="link_titulo"><h1>Video Trend</h1></a>
+        </div>
         <div class="usuario_home_page" id="seccion_cabecera_usuario">
             <a href="URL_DEL_ENLACE">
                 <img src="<?php echo base_url('Taller-web-tp3/appweb/public/assets/img/confi.png'); ?>" width="50px" class="imagen_usuario">
@@ -29,9 +27,49 @@
             </a>
         </div>
     </header>
+
     <div class="campos_busqueda">
-        <!-- Campos de búsqueda -->
+        <input type="text" id="tituloPelicula" placeholder="Título de la película" name="tituloPelicula">
+        <input type="text" id="categoriaPelicula" placeholder="Categoría de la película" name="categoriaPelicula">
+        <button onclick="buscarPelicula()">Buscar</button>
     </div>
+
+    <h1 >RESULTADOS DE LA BÚSQUEDA</h1>
+    <div class="seccion_resultados_busqueda" id="seccion_resultados_busqueda">
+        <div class="seccion_video" id="seccion_video_busqueda">
+            <div id="example_busqueda" class="tabla-videos">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Año</th>
+                            <th>Género</th>
+                            <th>Espectadores</th>
+                            <th>Codigo</th>
+                            <th>Ver Comentarios</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (isset($results)) : ?>
+                            <?php foreach ($results as $movie) : ?>
+                                <tr>
+                                    <td><?php echo $movie->movie->title; ?></td>
+                                    <td><?php echo $movie->movie->year; ?></td>
+                                    <td><?php echo $movie->movie->ids->slug; ?></td>
+                                    <td><?php echo $movie->watchers; ?></td>
+                                    <td><?php echo $movie->movie->ids->imdb; ?></td>
+                                    <td>
+                                        <a href="<?php echo base_url('../Taller-web-tp3/appweb/public/TraktController/verComentarios/' . $movie->movie->ids->trakt); ?>">Ver Comentarios</a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
     <h1 >PELICULAS RECOMENDADAS</h1>
     <div class="seccion_resultados" id="seccion_resultados">
         <div class="seccion_video" id="seccion_video">
@@ -64,13 +102,16 @@
                 </table>
             </div>
         </div>
-        <button onclick="paggination()">Buscar Más</button>
     </div>
+
+    <button onclick="paggination()">Buscar Más</button>
+
     <h2 style="text-align: center;">Mis categorias:</h2>
     <div class="seccion_mis_categorias">
         <div class="lista_categorias" id="lista_categorias">
         </div>
     </div>
+
     <script src="<?php echo base_url('/assets/js/home_page_script.js'); ?>"></script>
 </body>
 </html>
